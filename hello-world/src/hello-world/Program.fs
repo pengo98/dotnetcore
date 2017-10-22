f05a21f1991b4327bcf10aae687f01243ce66112
nelson.lin <nelson.lin@jet.com>
2017-10-21 22:56:59 -0400

    hello-world

---
+++ hello-world/src/hello-world/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#! Arg=%A DateTime=%A foo=%s" argv (DateTimeOffset.UtcNow) "bar"
+    0 // return an integer exit code
